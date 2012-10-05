de5939878006c7788231400595f5888a5430dfee
Arjen Vrielink <arjenvrielink@gmail.com>
2012-10-05 18:47:13 +0200

    virus looks for target stations within radius of X km and prepares to attack

--- static/js/main.js
+++ static/js/main.js
@@ -63,11 +63,14 @@ require(['jquery', 'bootstrap', 'openlayers', 'stamen', 'utrechtspoor', 'station
     selectStation.activate();
 
     /*
-     * LAYER EVENTS
+     * MAP & LAYER EVENTS
      */
 
     // bind mouseclick event to station layer and fire callback function
     stationLayer.events.register('featureselected', this, getSelectedGeometry);
+
+    // map opschonen als die veranderd want dan kloppen de raphael coordination niet meer
+    map.events.register('move', this, function (e) {paper.clear();});
 
     /*
      * RAPHAEL SETUP
@@ -84,12 +87,15 @@ require(['jquery', 'bootstrap', 'openlayers', 'stamen', 'utrechtspoor', 'station
      * THIS IS WHERE THE MAGIC HAPPENS
      */
 
+    // ugly declaration of global vars
+    var infectedFeatures = []; // array with stations already infected
+
     // function to get a geometry from an event and pass it on
     function getSelectedGeometry (e) {
         //console.log(e); // to check what is available in the event object: the feature, feature layer, should be xy but disabled by default in current OL
         var geometry = e.feature.geometry;
         bombStation(geometry);
-        virusSpread(geometry);
+        virusSpread(e.feature);
     }
 
     // function that animates explosion at station position
@@ -100,19 +106,37 @@ require(['jquery', 'bootstrap', 'openlayers', 'stamen', 'utrechtspoor', 'station
 
         // en dan nu wat magische raphael shizzle
         // draw something cool at the station position
-        var circle = paper.circle(xy.x, xy.y, 20);
-        circle.attr("fill", "#f00");
-        circle.attr("stroke", "#fff");
+        var circle = paper.circle(xy.x, xy.y, 10).animate({fill: "#f00", stroke: "#f99", "stroke-width": 10, "stroke-opacity": 0.6, opacity: 0.9}, 2500, "linear");
     }
 
     // function that finds the nearest stations and drops a zombiebomb
     // from the set of nearest stations, randomly n - 1 are selected and a new outbreak is calculated
-    function virusSpread (geometry) {
+    function virusSpread (feature) {
         console.log('virus spreading');
-        // find closest geometries in layer
+        // find features within x km from source feature 
+        // get bounds + 30 km and see which feature geometries from layer intersect
+        var searchRadius = 30000; // search Radius in m
+        var sourceBounds = feature.geometry.bounds;
+        searchBounds = new OpenLayers.Bounds(sourceBounds.left - searchRadius, sourceBounds.bottom - searchRadius, sourceBounds.right + searchRadius, sourceBounds.top + searchRadius);
 
-        var centroid = geometry.getCentroid();
-        var lonLat = new OpenLayers.LonLat(centroid.x, centroid.y);
-        var xy = map.getPixelFromLonLat(lonLat);
+        var targetFeatures = [];
+        var infectedFeatures.push(feature); // list of features already infected, can be skipped
+        for (i=0; i < feature.layer.features.length; i++) {
+            targetFeature = feature.layer.features[i];
+            // check if intersects and add to targetlist
+            if (searchBounds.intersectsBounds(targetFeature.geometry.bounds)) {
+                console.log('added target' + i + '; calculating distance');
+                //calculate distance to target
+                var distance = feature.geometry.distanceTo(targetFeature.geometry);
+                console.log(distance + ' m');
+                targetFeatures.push(targetFeature);
+            }
+        }
+        console.log(targetFeatures.length + ' targets found; attacking');
+
+        // loop over targets not yet infected; intersect targetFeatures array with infectedFeatures
+
+        // if targetList = empty: return with 'all targets completed'
+
     }
 }); // close require()
